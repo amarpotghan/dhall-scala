3bd6373913696457c429154e7ac1c8a0824cbaac
Amar Potghan <amar@riskident.com>
2017-03-18 15:45:32 +0100

    Add unit tests for shiftVariableIndices (#6)

---
+++ src/test/scala/ExprSpec.scala
@@ -0,0 +1,47 @@
+package dhall
+
+import org.specs2.mutable._
+
+
+import Expr._
+
+class ExprSpec extends Specification {
+
+  "shiftVariableIndices: should shift index if provided variable name matches with, " >> {
+    "Lam domain label" >> {
+      // \x: IntegerType -> x
+      val given = Lam("x", IntegerType, Var("x", 0))
+      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      shifted mustEqual Lam("x", IntegerType, Var("x", 1))
+    }
+
+    "Let binding label" >> {
+      // let x = 1 in x
+      val given = Let("x", None, IntegerLit(1), Var("x", 0))
+      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      shifted mustEqual Let("x", None, IntegerLit(1), Var("x", 1))
+    }
+
+    "Var name" >> {
+      // x
+      val given = Var("x", 0)
+      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      shifted mustEqual Var("x", 1)
+    }
+
+    "Quant label" >> {
+      // forall(a: Int) -> a
+      val given = Quant("x", IntegerType, Var("x", 0))
+      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      shifted mustEqual Quant("x", IntegerType, Var("x", 1))
+    }
+
+    "Var in Lam in Let" >> {
+      // let x = True in \x: BoolType -> x && True
+      val given = Let("x", Some(BoolType), BoolLit(true), Lam("x", BoolType, BoolAnd(Var("x", 0), BoolLit(true))))
+      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      val expected = Let("x", Some(BoolType), BoolLit(true), Lam("x", BoolType, BoolAnd(Var("x", 1), BoolLit(true))))
+      shifted mustEqual expected
+    }
+  }
+}
