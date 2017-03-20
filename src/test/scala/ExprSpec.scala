60c7898b3a829440e6d08daa6c40c8b5f0fe923a
Amar Potghan <amar@riskident.com>
2017-03-20 17:57:11 +0100

    Move shiftVariableIndices in Expr trait (#6)

--- src/test/scala/ExprSpec.scala
+++ src/test/scala/ExprSpec.scala
@@ -11,35 +11,35 @@ class ExprSpec extends Specification {
     "Lam domain label" >> {
       // \x: IntegerType -> x
       val given = Lam("x", IntegerType, Var("x", 0))
-      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      val shifted = given.shiftVariableIndices(1, Var("x", 0))
       shifted mustEqual Lam("x", IntegerType, Var("x", 1))
     }
 
     "Let binding label" >> {
       // let x = 1 in x
       val given = Let("x", None, IntegerLit(1), Var("x", 0))
-      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      val shifted = given.shiftVariableIndices(1, Var("x", 0))
       shifted mustEqual Let("x", None, IntegerLit(1), Var("x", 1))
     }
 
     "Var name" >> {
       // x
       val given = Var("x", 0)
-      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      val shifted = given.shiftVariableIndices(1, Var("x", 0))
       shifted mustEqual Var("x", 1)
     }
 
     "Quant label" >> {
       // forall(a: Int) -> a
       val given = Quant("x", IntegerType, Var("x", 0))
-      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      val shifted = given.shiftVariableIndices(1, Var("x", 0))
       shifted mustEqual Quant("x", IntegerType, Var("x", 1))
     }
 
     "Var in Lam in Let" >> {
       // let x = True in \x: BoolType -> x && True
       val given = Let("x", Some(BoolType), BoolLit(true), Lam("x", BoolType, BoolAnd(Var("x", 0), BoolLit(true))))
-      val shifted = Expr.shiftVariableIndices(1, Var("x", 0), given)
+      val shifted = given.shiftVariableIndices(1, Var("x", 0))
       val expected = Let("x", Some(BoolType), BoolLit(true), Lam("x", BoolType, BoolAnd(Var("x", 1), BoolLit(true))))
       shifted mustEqual expected
     }
